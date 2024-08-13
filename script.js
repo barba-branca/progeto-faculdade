d48dcf13a729703f0b47b695d89ce53bafc816f9
KAUE NEVES <137438983+barba-branca@users.noreply.github.com>
2024-08-13 20:50:54 -0300

    projeto da faculdade

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('executeBtn').addEventListener('click', function() {
+    const sqlInput = document.getElementById('sqlImput').value;
+    const resultDiv = document.getElementById('result';
+        //Simulaçao basica de execuçao SQL
+        if(sqlInput.toLowerCase() .include('select')) {
+            
+        }
+    )
+})
